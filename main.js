b2a8f532a203f713aa3d27a95efe22015604ba80
enantes <sambaeverything@gmail.com>
2022-05-05 22:03:33 +0200

    Finish project Navigatio

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Print out into the console the entire SOCBook object
 console.log(SOCBook);
-let mangoLovers = [];
+// let mangoLovers = [];
 let mangoLoversAddress = [];
 let userEmail;
 
@@ -100,25 +100,32 @@ console.log("What I found:", found);
 // Make a list containing only the people's names who's favourite fruit is a mango
 
 function getMangoLovers() {
-  
+  let mangoLovers = [];
   let fruit = 'mango';
 // Using Includes Method:
 for (i=0; i < SOCBook.data.people.length; i++) {
-if (SOCBook.data.people[i].favoriteFruit.includes(fruit)) {
-      console.log (' Name of fruit  ');
-      mangoLovers.push(SOCBook.data.people[i].name)
-      console.log('mango lovers are: ' + mangoLovers)
-    }}
+  if (SOCBook.data.people[i].favoriteFruit === fruit){
+    mangoLovers.push(SOCBook.data.people[i].name)
+    
+  }
   
-
-  return mangoLovers;
+// if (SOCBook.data.people[i].favoriteFruit.includes(fruit)) {
+//       console.log (' Name of fruit  ');
+//       mangoLovers.push(SOCBook.data.people[i].name)
+//       console.log('mango lovers are: ' + mangoLovers)
+//     } 
+}
+return mangoLovers;
 }
 
 console.log("Who loves mango?", getMangoLovers());
 
 // Task 6
-// Complete the function. Try and return the address of the person in the position in the array we've been handed
-// SOCBook.data.people[1]['full address']
+// Complete the function. Try and return the address of 
+//the person in the position in the array we've been handed
+
+// list
+
 
 // for (i=0; i<mangoLovers.length; i++){
 //   getAddress(mangoLovers[i])
@@ -126,29 +133,51 @@ console.log("Who loves mango?", getMangoLovers());
 
 // mangoLovers = [name1, name2, name3] The people that love mango  
 
+// Task 6
+// Complete the function. Try and return the address of the person 
+//in the position in the array we’ve been handed
 function getAddress(positionInArray) {
   // Your code goes here...
-  // What happens if they have the SAME NAME?!
-
-//   for (i=0; i < mangoLovers.length; i++)
-//   {
-//     if 
-//   }
-  
-//   =0; i < SOCBook.data.people.length; i++) {
-//     if (SOCBook.data.people[i].name.includes()) {
-//           console.log (' Name of fruit  ');
-//           mangoLovers.push(SOCBook.data.people[i].name)
-//           console.log('mango lovers are: ' + mangoLovers)
-//         }}
-    
-
+  // first assign a variable SOCBook.data.people[i]
+  //
+  let personsAddress =  SOCBook.data.people[positionInArray]['full address'];
+  return personsAddress;
 
 }
+console.log(getAddress(7));
 
-console.log(getAddress(0));
 
 // Task 7
 // Show a profile for each person in the ul with id "profiles"
 // A profile should include their name, age, and their image
 // Remember, break this down and tackle it step by step
+// Lets give a profile to EVERYONE because we are fair like that 
+//pull name, age and 
+// create a funtion profile that takes in index and creates a profile
+
+for(i=0; i<SOCBook.data.people.length; i++){
+  createProfile(i);
+}
+
+
+
+function createProfile (index) {
+  let name = SOCBook.data.people[index].name;
+  let age = SOCBook.data.people[index].age;
+  let image = SOCBook.data.people[index].picture;
+  let personUl = document.querySelector('#profiles');
+  let newLi = document.createElement('li');
+  personUl.appendChild(newLi);
+  newLi.innerText = name;
+  let ageElement = document.createElement('p');
+  personUl.appendChild(ageElement);
+  ageElement.innerText = age;
+
+  let imageElement = document.createElement('img');
+  personUl.appendChild(imageElement);
+  imageElement.src = image;  //`<image>src="${image}"</image>`;
+  
+
+}
+
+
